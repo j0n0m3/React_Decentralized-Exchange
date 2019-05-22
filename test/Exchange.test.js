c2505d410af262b0b4d3eed4a3bef89314669602
norenaja <jnorena.jr@gmail.com>
2019-05-21 23:31:50 -0400

    exchange testing

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -7,7 +7,7 @@ require('chai')
   .use(require('chai-as-promised'))
   .should();
 
-contract('Exchange', ([deployer, feeAccount, user1]) => {
+contract('Exchange', ([deployer, feeAccount, user1, user2]) => {
   let token;
   let exchange;
   let feePercent = 10;
@@ -282,14 +282,107 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
     beforeEach(async () => {
       // user1 deposits Ether
       await exchange.depositEther({ from: user1, value: ether(1) });
+      // give tokens to user2
+      await token.transfer(user2, tokens(100), { from: deployer });
+      // user2 deposits tokens only
+      await token.approve(exchange.address, tokens(2), { from: user2 });
+      await exchange.depositToken(token.address, tokens(2), { from: user2 });
       // user1 makes an order to buy tokens with Ether
       await exchange.makeOrder(
         token.address,
-        tokens(10),
+        tokens(1),
         ETHER_ADDRESS,
         ether(1),
         { from: user1 }
       );
+    });
+
+    describe('filling orders', async () => {
+      let result;
+
+      describe('success', async () => {
+        beforeEach(async () => {
+          // user2 fills order
+          result = await exchange.fillOrder('1', { from: user2 });
+        });
+
+        it('executes the trade & charge fees', async () => {
+          let balance;
+          balance = await exchange.balanceOf(token.address, user1);
+          balance
+            .toString()
+            .should.equal(tokens(1).toString(), 'user1 received tokens');
+          balance = await exchange.balanceOf(ETHER_ADDRESS, user2);
+          balance
+            .toString()
+            .should.equal(ether(1).toString(), 'user2 received Ether');
+          balance = await exchange.balanceOf(ETHER_ADDRESS, user1);
+          balance.toString().should.equal('0', 'user1 Ether deducted');
+          balance = await exchange.balanceOf(token.address, user2);
+          balance
+            .toString()
+            .should.equal(
+              tokens(0.9).toString(),
+              'user2 tokens deducted with fee applied'
+            );
+          const feeAccount = await exchange.feeAccount();
+          balance = await exchange.balanceOf(token.address, feeAccount);
+          balance
+            .toString()
+            .should.equal(tokens(0.1).toString(), 'feeAccount received fee');
+        });
+
+        it('updates filled orders', async () => {
+          const orderFilled = await exchange.orderFilled(1);
+          orderFilled.should.equal(true);
+        });
+
+        it('emits an "Trade" event', async () => {
+          const log = result.logs[0];
+          log.event.should.eq('Trade');
+          const event = log.args;
+          event.id.toString().should.equal('1', 'id is correct');
+          event.user.should.equal(user1, 'user is correct');
+          event.tokenGet.should.equal(token.address, 'tokenGet is correct');
+          event.amountGet
+            .toString()
+            .should.equal(tokens(1).toString(), 'amountGet is correct');
+          event.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGive is correct');
+          event.amountGive
+            .toString()
+            .should.equal(ether(1).toString(), 'amountGive is correct');
+          event.timestamp
+            .toString()
+            .length.should.be.at.least(1, 'timestamp is present');
+        });
+      });
+
+      describe('failure', async () => {
+        it('rejects invalid order ids', async () => {
+          const invalidOrderId = 99999;
+          await exchange
+            .fillOrder(invalidOrderId, { from: user2 })
+            .should.be.rejectedWith(EVM_REVERT);
+        });
+
+        it('rejects already-filled orders', async () => {
+          // Fill the order
+          await exchange.fillOrder('1', { from: user2 }).should.be.fulfilled;
+          // Try to fill it again
+          await exchange
+            .fillOrder('1', { from: user2 })
+            .should.be.rejectedWith(EVM_REVERT);
+        });
+
+        it('rejects cancelled orders', async () => {
+          // Cancel the order
+          await exchange.cancelOrder('1', { from: user1 }).should.be.fulfilled;
+          // Try to fill the order
+          await exchange
+            .fillOrder('1', { from: user2 })
+            .should.be.rejectedWith(EVM_REVERT);
+        });
+      });
     });
 
     describe('cancelling orders', async () => {
@@ -304,10 +397,41 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
           const orderCancelled = await exchange.orderCancelled(1);
           orderCancelled.should.equal(true);
         });
+
+        it('emits a "Cancel" event', async () => {
+          const log = result.logs[0];
+          log.event.should.eq('Cancel');
+          const event = log.args;
+          event.id.toString().should.equal('1', 'id is correct');
+          event.user.should.equal(user1, 'user is correct');
+          event.tokenGet.should.equal(token.address, 'tokenGet is correct');
+          event.amountGet
+            .toString()
+            .should.equal(tokens(1).toString(), 'amountGet is correct');
+          event.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGive is correct');
+          event.amountGive
+            .toString()
+            .should.equal(ether(1).toString(), 'amountGive is correct');
+          event.timestamp
+            .toString()
+            .length.should.be.at.least(1, 'timestamp is present');
+        });
       });
 
       describe('failure', async () => {
-        //
+        it('rejects invalid order ids', async () => {
+          const invalidOrderId = 9999;
+          await exchange
+            .cancelOrder(invalidOrderId, { from: user1 })
+            .should.be.rejectedWith(EVM_REVERT);
+        });
+
+        it('rejects unauthorized cancelations', async () => {
+          // Try to cancel order from another user
+          await exchange
+            .cancelOrder('1', { from: user2 })
+            .should.be.rejectedWith(EVM_REVERT);
+        });
       });
     });
   });
